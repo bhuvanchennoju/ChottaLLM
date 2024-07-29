d0199dfae20d0b260125f896255706b796aa74d4
bhuvanchennoju <bhuvankumarchennoju@gmail.com>
2024-07-28 22:07:37 -0500

    minor data changes

--- src/data.py
+++ src/data.py
@@ -4,3 +4,11 @@ Date: 27th july 2024
 Description: This file have the data processing general functions.
 
 """
+
+import numpy as np
+
+def load_data(file_path):
+    pass
+
+def save_data(file_path, data):
+    pass
