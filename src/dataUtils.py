6ab742f8e10924fceb81f46915a783aa570a4ca4
bhuvanchennoju <bhuvankumarchennoju@gmail.com>
2024-08-04 21:53:43 -0500

    attention in dev notebook

--- src/dataUtils.py
+++ src/dataUtils.py
@@ -10,7 +10,7 @@ Description: All the required function to prepare the data for the model trainin
 
 from tiktoken import get_encoding
 import numpy as np
-from torch import tensor,stack, int64
+from torch import tensor,stack, int64,long
 from torch.nn.utils.rnn import pad_sequence
 
 class customTokenizer:
@@ -19,6 +19,11 @@ class customTokenizer:
             self.encoder = get_encoding(encoding) 
         except:
             raise Exception('Tokenizer not found, check the tokenizer name in tiktoken library')
+        
+        self.eot = self.encoder.eot_token
+        
+    def get_vocab_size(self):
+        return self.encoder.n_vocab
 
     def encode(self,doc):
         tokens = [self.eot]
@@ -27,6 +32,8 @@ class customTokenizer:
     
     def decode(self,tokens):
         return self.encoder.decode(tokens)
+    
+
 
     def encode_np(self,doc):
         """
@@ -64,6 +71,9 @@ class CustomDataset:
 
     def _load_data(self):
         self.data = np.load(self.data_dir)
+        if self.data.dtype == np.int16:
+            self.data = self.data.astype(np.int32)
+        self.data = tensor(self.data,dtype = long) # converting to tensor
         return self.data
     
     def __len__(self):
@@ -86,6 +96,10 @@ class CustomDataset:
 
 
 class CustomDataloader:
+
+    """
+    purely memory based dataloader
+    """
 
     def __init__(self,dataset:CustomDataset,batch_size = 64, shuffle = True, pad = False):
         self.dataset = dataset
@@ -128,9 +142,76 @@ class CustomDataloader:
         return len(self.dataset) // self.batch_size
     
 
+class DataLoaderLite:
+    def __init__(self, B, T, process_rank, num_processes, split):
+        self.B = B
+        self.T = T
+        self.process_rank = process_rank # what is this for? t
+        self.num_processes = num_processes
+        assert split in {'train', 'val'}
+
+        # get the shard filenames
+        data_root = "edu_fineweb10B"
+        shards = os.listdir(data_root)
+        shards = [s for s in shards if split in s]
+        shards = sorted(shards)
+        shards = [os.path.join(data_root, s) for s in shards]
+        self.shards = shards
+        assert len(shards) > 0, f"no shards found for split {split}"
+        if master_process:
+            print(f"found {len(shards)} shards for split {split}")
+        self.reset()
+
+    def reset(self):
+        # state, init at shard zero
+        self.current_shard = 0
+        self.tokens = load_tokens(self.shards[self.current_shard])
+        self.current_position = self.B * self.T * self.process_rank
+
+    def next_batch(self):
+        B, T = self.B, self.T
+        buf = self.tokens[self.current_position : self.current_position+B*T+1]
+        x = (buf[:-1]).view(B, T) # inputs
+        y = (buf[1:]).view(B, T) # targets
+        # advance the position in the tensor
+        self.current_position += B * T * self.num_processes
+        # if loading the next batch would be out of bounds, advance to next shard
+        if self.current_position + (B * T * self.num_processes + 1) > len(self.tokens):
+            self.current_shard = (self.current_shard + 1) % len(self.shards)
+            self.tokens = load_tokens(self.shards[self.current_shard])
+            self.current_position = B * T * self.process_rank
+        return x, y
 
 
-    
+
+class ShardedDataloader:
+    def __init__(self,B:int,T:int,process_rank:int,num_processes:int,split:str):
+        """
+        B: int, batch size
+        T: int, sequence length or context window size
+        process_rank: int, the rank of the process only for the distributed training
+        num_processes: int, the number of processes for the distributed training
+        split: str, 'train' or 'val'
+    """
+
+
+        self.B = B
+        self.T = T
+        self.process_rank = process_rank
+        self.num_processes = num_processes
+        assert split in {'train','val'}
+
+        data_root = "edu_fineweb10B"
+        shards = os.listdir(data_root)
+        shards = [s for s in shards if split in s]
+        shards = sorted(shards)
+        shards = [os.path.join(data_root,s) for s in shards]
+        self.shards = shards
+        assert len(shards) > 0, f"no shards found for split {split}"
+        if master_process:
+            print(f"found {len(shards)} shards for split {split}")
+        self.reset()
+
 
 
 
--- src/model.py
+++ src/model.py
@@ -3,6 +3,9 @@
 Author: Bhuvan Chennoju 
 Date: 1st August 2024
 
+kudos to:
+    - Karpathy's: nanogpt repo 
+    https://github.com/karpathy/nanoGPT/blob/master/model.py
 
 """
 
@@ -28,6 +31,39 @@ class CausalSelfAttention(nn.Module):
 
         # flash attention parameters
         self.flash = hasattr(torch.nn.functional, 'scaled_dot_product_attention')
-        
+        if self.flash:
+            self.flash_attention = torch.nn.functional.scaled_dot_product_attention
+        else:
+            print('Using standard attention, as flash attention is only available in PyTorch >= 2.0.0')
+            print(torch.__version__)
+            ## register buffer that will be used for the attention mask
+            self.register_buffer('bias', torch.tril(torch.ones(config.block_size, config.block_size)).view(1, 1, config.block_size, config.block_size))
+            
+    
+    def forward(self,x):
+        B,T,C = x.size()  # B: batch size, T: sequence length, C: n_embd
+
+        q,k,v = self.c_attn(x).split(self.n_embd, dim = 2) # split the output of the linear layer into q,k,v
+        q = q.view(B,T,self.n_head, C//self.n_head).transpose(1,2) # reshape q to (B,n_head,T,C//n_head)
+        k = k.view(B,T,self.n_head, C//self.n_head).transpose(1,2)
+        v = v.view(B,T,self.n_head, C//self.n_head).transpose(1,2)
+
+        # flash attention (B,nh T, C//n_head) X (B,nh, C//n_head, T) -> (B,nh,T,T)
+
+        if self.flash:
+            y = self.flash_attention(q, k, v, attn_mask = None, dropout_p=self.dropout if self.training else 0, is_causal=True)
+        else:
+            att = (q @ k.transpose(-2,-1)) * (1.0 / math.sqrt(k.size(-1)))
+            att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
+            att = F.softmax(att, dim=-1)
+            att = self.attn_dropout(att)
+            y = att @ v # (B,n_head,T,T) X (B,n_head,T,C//n_head) -> (B,n_head,T,C//n_head)
+
+        y = y.transpose(1,2).contiguous().view(B,T,C) # reshape y to (B,T,C)
+
+        y = self.resid_dropout(self.c_proj(y)) # apply residual dropout and project the output of attention to n_embd
+        return y
+    
 
 
+    class LayerNorm(nn.)
